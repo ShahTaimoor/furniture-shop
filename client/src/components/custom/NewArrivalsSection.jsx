1756012ec69035e6a0757739aaee01382063be78
ShahTaimoor <taimour448@gmail.com>
2026-08-18 16:07:57 +0500

    refactor: Update slidesPerView settings for BestSellerSection and NewArrivalsSection for improved responsiveness

--- client/src/components/custom/NewArrivalsSection.jsx
+++ client/src/components/custom/NewArrivalsSection.jsx
@@ -122,7 +122,10 @@ const NewArrivalsSection = () => {
             640: { slidesPerView: 3 },
             768: { slidesPerView: 3.5 },
             1024: { slidesPerView: 4 },
-            1280: { slidesPerView: 4 },
+            1280: { slidesPerView: 5 },
+            1536: { slidesPerView: 6 },
+            1800: { slidesPerView: 7 },
+            2200: { slidesPerView: 8 },
           }}
           slidesPerView={1.5}
           onSlideChange={(swiper) => {
